49628bafc1abb64163be2e080c138b2b37044e5a
tapeesh <tapeeshsood@gmail.com>
2016-09-27 12:01:54 +0800

    Update interface.js

--- js/autocompaste/interface.js
+++ js/autocompaste/interface.js
@@ -140,7 +140,7 @@ AutoComPaste.Interface = (function () {
 
         privates.wm.createWindow("text_editor");
         privates.wm.setWindowTitle("text_editor", "Text Editor");
-        privates.wm.setWindowContent('text_editor', 'Hi there' + "\n", acp_textarea );
+        privates.wm.setWindowContent('text_editor', acp_textarea, 'Hi there' );
         acp_textarea.focus();
 
         // Dispatch an event.
