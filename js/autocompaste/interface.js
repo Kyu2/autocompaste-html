d95b2e6337d07f9782710edaa00aa6d562fcb549
tapeesh <tapeeshsood@gmail.com>
2016-09-28 23:54:12 +0800

    Update interface.js

--- js/autocompaste/interface.js
+++ js/autocompaste/interface.js
@@ -165,7 +165,7 @@ AutoComPaste.Interface = (function () {
        }
         
         if (privates.activity == 'same_document'){
-        privates.wm.createWindow("text_editor", 800,400);
+        privates.wm.createWindow("text_editor", 1100,400);
         privates.wm.setWindowTitle("text_editor", "Text Editor");
         var new_content =  privates.texts['Data 1'];
         privates.lines_to_highlight.map (function (value, index, array) {
